0755162ecfa03250ad93e66b9678fc038dc9fb8e
pkabelka <pkabelka@email.cz>
2023-04-24 18:20:05 +0200

    fixed printing dirs that appeared at same depth

--- paths2tree.go
+++ paths2tree.go
@@ -6,11 +6,13 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"github.com/pkabelka/litu"
 )
 
 type PathHist struct {
 	depth int
-	dir string
+	path []string
 }
 
 func main() {
@@ -28,19 +30,23 @@ func main() {
 		if depth > 0 && splitPath[len(splitPath)-2] != lastDir {
 			found := false
 			for _, e := range pathHist {
-				if e.depth == depth && e.dir == splitPath[len(splitPath)-2] {
+				if e.depth == depth && litu.Equal(e.path, splitPath[:len(splitPath)-1]) {
 					found = true
 				}
 			}
 			if !found {
 				fmt.Printf("%s%s\n", strings.Repeat("|   ", depth-1), splitPath[len(splitPath)-2])
 				lastDir = splitPath[len(splitPath)-2]
-				pathHist = append(pathHist, PathHist{depth, lastDir})
+				pathHist = append(pathHist, PathHist{depth, splitPath[:len(splitPath)-1]})
 			}
 		}
 
 		treePrefix := strings.Repeat("|   ", depth)
 		fmt.Printf("%s%s\n", treePrefix, splitPath[len(splitPath)-1])
+
+		if depth > 0 {
+			lastDir = splitPath[len(splitPath)-2]
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
