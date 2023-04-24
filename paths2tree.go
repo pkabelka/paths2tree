ff076bc5b9252378ee88f5629d7fc69c3ca14ec2
pkabelka <pkabelka@email.cz>
2023-04-24 19:18:27 +0200

    use a channel to send tree level to main instead of printing

--- paths2tree.go
+++ paths2tree.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -10,46 +11,69 @@ import (
 	"github.com/pkabelka/litu"
 )
 
-type PathHist struct {
-	depth int
-	path []string
+type Path2TreeRes struct {
+	TreeLevel string
+	Err error
 }
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+func Paths2Tree(r io.Reader) <-chan Path2TreeRes {
+	type PathHist struct {
+		depth int
+		path []string
+	}
+
+	scanner := bufio.NewScanner(r)
 
 	depth := 0
 	var lastDir string
 	pathHist := make([]PathHist, 0, 64)
+	treeLevel := ""
+	ch := make(chan Path2TreeRes)
 
-	for scanner.Scan() {
-		path := scanner.Text()
-		splitPath := strings.Split(path, "/")
-		depth = len(splitPath)-1
+	go func() {
+		defer close(ch)
+		for scanner.Scan() {
+			treeLevel = ""
+			path := scanner.Text()
+			splitPath := strings.Split(path, "/")
+			depth = len(splitPath)-1
 
-		if depth > 0 && splitPath[len(splitPath)-2] != lastDir {
-			found := false
-			for _, e := range pathHist {
-				if e.depth == depth && litu.Equal(e.path, splitPath[:len(splitPath)-1]) {
-					found = true
+			if depth > 0 && splitPath[len(splitPath)-2] != lastDir {
+				found := false
+				for _, e := range pathHist {
+					if e.depth == depth && litu.Equal(e.path, splitPath[:len(splitPath)-1]) {
+						found = true
+					}
+				}
+				if !found {
+					treeLevel += fmt.Sprintf("%s%s\n", strings.Repeat("|   ", depth-1), splitPath[len(splitPath)-2])
+					lastDir = splitPath[len(splitPath)-2]
+					pathHist = append(pathHist, PathHist{depth, splitPath[:len(splitPath)-1]})
 				}
 			}
-			if !found {
-				fmt.Printf("%s%s\n", strings.Repeat("|   ", depth-1), splitPath[len(splitPath)-2])
+
+			treePrefix := strings.Repeat("|   ", depth)
+			treeLevel += fmt.Sprintf("%s%s\n", treePrefix, splitPath[len(splitPath)-1])
+
+			if depth > 0 {
 				lastDir = splitPath[len(splitPath)-2]
-				pathHist = append(pathHist, PathHist{depth, splitPath[:len(splitPath)-1]})
 			}
+			ch <- Path2TreeRes{treeLevel, nil}
 		}
-
-		treePrefix := strings.Repeat("|   ", depth)
-		fmt.Printf("%s%s\n", treePrefix, splitPath[len(splitPath)-1])
-
-		if depth > 0 {
-			lastDir = splitPath[len(splitPath)-2]
+		if err := scanner.Err(); err != nil {
+			ch <- Path2TreeRes{"", err}
 		}
-	}
+	}()
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	return ch
+}
+
+func main() {
+	ch := Paths2Tree(os.Stdin)
+	for paths2TreeRes := range ch {
+		if paths2TreeRes.Err != nil {
+			log.Fatal(paths2TreeRes.Err)
+		}
+		fmt.Print(paths2TreeRes.TreeLevel)
 	}
 }
